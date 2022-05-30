45127ecbf92ec88c0bdfc3d123c52de862d4cfea
Olga_Michalyata <olgamikhalyata.com@mail.ru>
2022-05-30 02:02:13 +0200

    commit 30.05.2022

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: true, 
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slaids/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/slaids/chevron-right-solid.png"></button>',
         responsive: [
@@ -14,4 +14,11 @@ $(document).ready(function(){
             }
           ]
       });
-  });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+});
